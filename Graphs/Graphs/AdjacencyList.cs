1fbe32ccc4dc6c777c2afcc3fb224dc9d38c0db8
wcd7037 <wcd7037@NM2550-28>
2017-04-10 10:38:37 -0400

    sssss

---
+++ Graphs/Graphs/AdjacencyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    class AdjacencyList
+    {
+        Dictionary<string, List<string>> dict;
+
+        public AdjacencyList()
+        {
+            dict = new Dictionary<string, List<string>>();
+            dict.Add("kitchen", new List<string>{ "library", "bedroom", "dining room" });
+            dict.Add("bedroom", new List<string> { "kitchen" });
+            dict.Add("library", new List<string> { "bathroom", "kitchen" });
+            dict.Add("dining room", new List<string> { "kitchen", "exit", "hallway" });
+            dict.Add("bathroom", new List<string> { "library", "sunroom" });
+            dict.Add("sunroom", new List<string> { "bathroom", "hallway" });
+            dict.Add("hallway", new List<string> { "sunroom", "dining room" });
+            dict.Add("exit", new List<string> { "dining room" });
+        }
+
+        public List<String> GetAdjacentList(string room)
+        {
+            return dict[room];
+        }
+
+        public bool IsConnected(string room1, string room2)
+        {
+            if (dict[room1].Contains(room2))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Graphs/Graphs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdjacencyList list = new AdjacencyList();
+
+            string input = "";
+            //Starting room
+            string room = "library";
+
+            while(room != "exit")
+            {
+                Console.WriteLine("You are currently in the: " + room);
+                Console.Write("Nearby are: ");
+                foreach(string s in list.GetAdjacentList(room))
+                {
+                    Console.Write(s + "   ");
+                }
+                Console.WriteLine("\nWhere would you like to go? ");
+                input = Console.ReadLine();
+
+                if(list.IsConnected(room, input))
+                {
+                    room = input;
+                }
+                else
+                {
+                    Console.WriteLine("That ain't no room nearby!");
+                }
+            }
+
+            Console.WriteLine("Wow, great job, you did it...");
+        }
+    }
+}
