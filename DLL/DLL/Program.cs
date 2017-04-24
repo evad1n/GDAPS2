6e0e46d4c71ed117b28bff3bce6c5b768fe889a5
wcd7037 <wcd7037@NM2550-28>
2017-04-24 10:32:45 -0400

    im a real qizarrrd

---
+++ DLL/DLL/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PCL;
+
+namespace DLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+
+            Console.WriteLine(myMath.Add(a, b));
+        }
+    }
+}
